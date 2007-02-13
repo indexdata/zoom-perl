fbb39931afaadffa44e33e65ffabf96748e8873f
mike <mike>
2007-02-13 15:30:51 +0000

    v1.17

--- lib/Net/Z3950/ZOOM.pm
+++ lib/Net/Z3950/ZOOM.pm
@@ -1,4 +1,4 @@
-# $Id: ZOOM.pm,v 1.31 2006-12-06 11:18:33 mike Exp $
+# $Id: ZOOM.pm,v 1.32 2007-02-13 15:30:51 mike Exp $
 
 package Net::Z3950::ZOOM; 
 
@@ -6,7 +6,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '1.16';
+our $VERSION = '1.17';
 
 require XSLoader;
 XSLoader::load('Net::Z3950::ZOOM', $VERSION);
