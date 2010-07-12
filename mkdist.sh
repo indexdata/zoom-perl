bff7ae81d284313be9684c2ef6c06dfe18633845
Mike Taylor <mike@miketaylor.org.uk>
2010-07-12 17:14:30 +0100

    New file mkdist.sh, copied from mkadmin/mkdist.sh

---
+++ mkdist.sh
@@ -0,0 +1,16 @@
+# Creates a distribution tarball of the kind that Debian packages are
+# made from.
+DEBCHANGELOG=debian/changelog
+
+l=`head -1 $DEBCHANGELOG`
+NAME=`echo $l|sed 's/ .*//g'`
+FULLVERSION=`echo $l|sed 's/.*(//g'|sed 's/).*//g'`
+VERSION=`echo $FULLVERSION|sed 's/-.*//g'`
+
+git archive --format=tar --prefix=$NAME-$VERSION/ HEAD > $NAME-$VERSION.tar
+tar xf $NAME-$VERSION.tar
+rm -r $NAME-$VERSION/debian
+rm -r $NAME-$VERSION/archive
+tar cfz $NAME-$VERSION.tar.gz $NAME-$VERSION
+rm $NAME-$VERSION.tar
+rm -r $NAME-$VERSION
