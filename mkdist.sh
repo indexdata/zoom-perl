c726be37cde462c6e9ccd25b7c928342e388ceb7
Mike Taylor <mike@miketaylor.org.uk>
2010-08-11 12:19:50 +0100

    Replace local mkdist.sh with reference to global one.

--- mkdist.sh
+++ mkdist.sh
@@ -1,15 +1,3 @@
-# Creates a distribution tarball of the kind that Debian packages are
-# made from.
-DEBCHANGELOG=debian/changelog
-
-l=`head -1 $DEBCHANGELOG`
-NAME=`echo $l|sed 's/ .*//g'`
-FULLVERSION=`echo $l|sed 's/.*(//g'|sed 's/).*//g'`
-VERSION=`echo $FULLVERSION|sed 's/-.*//g'`
-
-git archive --format=tar --prefix=$NAME-$VERSION/ HEAD > $NAME-$VERSION.tar
-tar xf $NAME-$VERSION.tar
-rm -r $NAME-$VERSION/{debian,archive,modules}
-tar cfz $NAME-$VERSION.tar.gz $NAME-$VERSION
-rm $NAME-$VERSION.tar
-rm -r $NAME-$VERSION
+echo "Projects should no longer have their own mkdist.sh script"
+echo "Use the generic one in git-tools/id-deb-build/mkdist.sh"
+exit 1
